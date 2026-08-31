eee7f9d9836bf9de754c37f971d4448aaa4d39c6
patilpratibha9253-rgb <patilpratibha9253@gmail.com>
2026-08-31 22:49:52 +0530

    Add grade calculation function

---
+++ Assigenment 31  grades.py
@@ -0,0 +1,15 @@
+# grades.py
+
+def calculate_grade(average):
+    if average >= 90:
+        return "A+"
+    elif average >= 80:
+        return "A"
+    elif average >= 70:
+        return "B"
+    elif average >= 60:
+        return "C"
+    elif average >= 50:
+        return "D"
+    else:
+        return "F"
